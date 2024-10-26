3f98c97357e74e137e3ac9145dec4d044cb4e49f
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-10-26 11:49:29 -0400

    new project

---
+++ s07Autowiring/src/test/java/com/SpringLearning/s07Autowiring/S06DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringLearning.s07Autowiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S06DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
